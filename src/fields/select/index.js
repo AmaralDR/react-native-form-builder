8eab60cd36e493ccb3fee520b1805219b8552f38
Leonardo Alves <leo_alvessilva@hotmail.com>
2018-08-09 19:20:33 -0300

    Modificação para aparecer a bordar inferior interia

--- src/fields/select/index.js
+++ src/fields/select/index.js
@@ -57,23 +57,28 @@ export default class SelectField extends Component {
   render() {
     const { theme, attributes, ErrorComponent } = this.props;
     const selectedText = attributes.multiple ?
-    attributes.value.length || 'None' :
-    attributes.objectType ?
-    (attributes.value && attributes.value[attributes.labelKey]) || 'None'
-    : attributes.value || 'None';
+      attributes.value.length || 'None' :
+      attributes.objectType ?
+        (attributes.value && attributes.value[attributes.labelKey]) || 'None'
+        : attributes.value || 'None';
     return (
-      <View>
-        <ListItem icon onPress={() => this.toggleModalVisible()}>
+      <View style={{ flex: 1 }}>
+        <ListItem onPress={() => this.toggleModalVisible()} >
           <Icon color={theme.textInputIconColor} name={attributes.icon} style={attributes.iconStyle} />
           <Body>
             <Text style={{ ...attributes.labelStyle }}>{attributes.label}</Text>
           </Body>
           <Right>
-            <View style={{ width: deviceWidth / 2, alignItems: 'flex-end' }}>
-              <Text numberOfLines={1} ellipSizeMode="tail">{selectedText}</Text>
-            </View>
+            <View style={{
+              flexDirection: 'row',
+              alignItems: 'flex-start'
+            }}>
+              <View style={{ width: deviceWidth / 2, alignItems: 'flex-end' }}>
+                <Text>{selectedText}</Text>
+              </View>
 
-            <Icon name="ios-arrow-forward" />
+              <Icon style={{ marginLeft: 5 }} name="ios-arrow-forward" />
+            </View>
           </Right>
         </ListItem>
         <View style={{ paddingHorizontal: 15 }}>
@@ -101,33 +106,33 @@ export default class SelectField extends Component {
             </Header>
             <Content>
               {
-              attributes.options.map((item, index) => {
-                let isSelected = false;
-                if (attributes.multiple) {
-                  isSelected = attributes.objectType ?
-                  attributes.value.findIndex(option =>
-                    option[attributes.primaryKey] === item[attributes.primaryKey]
-                  ) !== -1 : (attributes.value.indexOf(item) !== -1);
-                }
-                return (
-                  <ListItem
-                    key={index}
-                    onPress={() => this.toggleSelect(item)}
-                  >
-                    { attributes.multiple &&
-                    <CheckBox
+                attributes.options.map((item, index) => {
+                  let isSelected = false;
+                  if (attributes.multiple) {
+                    isSelected = attributes.objectType ?
+                      attributes.value.findIndex(option =>
+                        option[attributes.primaryKey] === item[attributes.primaryKey]
+                      ) !== -1 : (attributes.value.indexOf(item) !== -1);
+                  }
+                  return (
+                    <ListItem
+                      key={index}
                       onPress={() => this.toggleSelect(item)}
-                      checked={isSelected}
-                    />
-                    }
-                    <Body>
-                      <Text style={{ paddingHorizontal: 5 }}>
-                        {attributes.objectType ? item[attributes.labelKey] : item }
-                      </Text>
-                    </Body>
-                  </ListItem>);
-              })
-            }
+                    >
+                      {attributes.multiple &&
+                        <CheckBox
+                          onPress={() => this.toggleSelect(item)}
+                          checked={isSelected}
+                        />
+                      }
+                      <Body>
+                        <Text style={{ paddingHorizontal: 5 }}>
+                          {attributes.objectType ? item[attributes.labelKey] : item}
+                        </Text>
+                      </Body>
+                    </ListItem>);
+                })
+              }
             </Content>
           </Container>
         </Modal>
